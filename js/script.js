fb126e33e38163ef92bfac6edfe26f0498a233c1
butterrflys3 <anggieta0809@gmail.com>
2025-08-25 16:28:21 +0700

    Mengubah kategori Premium jadi Pizza & perbaiki nama gambar , serta menuntaskan materi Part 4, mempelajari cara kerja JSON serta melakukan looping data untuk menampilkannya.

--- js/script.js
+++ js/script.js
@@ -1,25 +1,62 @@
-$.getJSON('data/pizza.json', function (data) {
-    let menu = data.menu;
-    $.each(menu, function (i, data) {
-        $('#daftarMenu').append(
-            '<div class="col-md-4">' +
-                '<div class="card mb-3">' +
-                    '<img src="img/' + data.gambar + '" class="card-img-top">' +
-                    '<div class="card-body">' +
-                        '<h5 class="card-title">' + data.nama_menu + '</h5>' +
-                        '<p class="card-text">' + data.deskripsi + '</p>' +
-                        '<h5 class="card-title">Rp. ' + data.harga + '</h5>' +
-                        '<a href="#" class="btn btn-primary">Order Now</a>' +
-                    '</div>' +
-                '</div>' +
-            '</div>'
-        );
-    });
-});
+function tampilkanSemuaMEnu() {
 
+    $.getJSON('data/pizza.json', function (data) {
+        let menu = data.menu;
+        $.each(menu, function (i, data) {
+            $('#daftarMenu').append(
+                '<div class="col-md-4">' +
+                    '<div class="card mb-3">' +
+                        '<img src="img/' + data.gambar + '" class="card-img-top">' +
+                        '<div class="card-body">' +
+                            '<h5 class="card-title">' + data.nama_menu + '</h5>' +
+                            '<p class="card-text">' + data.deskripsi + '</p>' +
+                            '<h5 class="card-title">Rp. ' + data.harga + '</h5>' +
+                            '<a href="#" class="btn btn-primary">Order Now</a>' +
+                        '</div>' +
+                    '</div>' +
+                '</div>'
+            );
+        });
+    });
+}
+
+    tampilkanSemuaMEnu();
 
 $('.nav-link').on('click', function() {
     $('.nav-link').removeClass('active');
-    $(this).addClass('active'); // ✅ benar
-});
+    $(this).addClass('active'); 
 
+    let kategori = $(this).html();
+    $('h1').html(kategori);
+
+    if (kategori == 'All Men') {
+        tampilkanSemuaMEnu();
+        return;
+    }
+
+    $.getJSON('data/pizza.json', function(data) {
+        let menu = data.menu;
+        let content = '';
+
+
+  $.each(menu, function(i, data){
+    console.log("Klik kategori:", kategori.toLowerCase().trim(), "| JSON:", data.kategori.toLowerCase().trim());
+
+    if (kategori.toLowerCase().trim() === "all menu" || data.kategori.toLowerCase().trim() === kategori.toLowerCase().trim()) {
+        content += `
+        <div class="col-md-4">
+            <div class="card mb-3">
+                <img src="img/${data.gambar}" class="card-img-top">
+                <div class="card-body">
+                    <h5 class="card-title">${data.nama_menu}</h5>
+                    <p class="card-text">${data.deskripsi}</p>
+                    <h5 class="card-title">Rp. ${data.harga}</h5>
+                    <a href="#" class="btn btn-primary">Order Now</a>
+                </div>
+            </div>
+        </div>`;
+    }
+});
+        $('#daftarMenu').html(content);
+    });
+});
